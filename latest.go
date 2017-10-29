56ae4712281cc35c29d33ad1071fb9521f1126d8
Arunvel Sriram <arunvelsriram@gmail.com>
2017-10-29 15:55:56 +0530

    Query ruby gems and return response

--- latest.go
+++ latest.go
@@ -1,11 +1,52 @@
 package main
 
-
 import (
   "fmt"
   "os"
+  "net/http"
+  "io/ioutil"
+  "encoding/json"
+  "time"
+  "log"
+
   "github.com/urfave/cli"
 )
+
+type Gem struct {
+  Name string
+  Version string
+}
+
+func latestRubyGemVersion(gemName string) Gem {
+  var gem Gem
+  var httpClient = &http.Client{
+    Timeout: time.Second * 10,
+  }
+
+  url := fmt.Sprintf("https://rubygems.org//api/v1/gems/%s.json", gemName)
+  resp, err := httpClient.Get(url)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  if(resp.StatusCode == http.StatusNotFound) {
+    return gem
+  }
+
+  jsonBlob, err := ioutil.ReadAll(resp.Body)
+  resp.Body.Close()
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  err = json.Unmarshal(jsonBlob, &gem)
+  if err != nil {
+    log.Print(gem)
+    log.Fatal(err)
+  }
+
+  return gem
+}
 
 func main() {
   var queryGem bool
@@ -31,6 +72,9 @@ func main() {
   app.Action = func(context *cli.Context) error {
     if queryGem {
       fmt.Println("gem")
+      gemName := context.Args().Get(0)
+      gem := latestRubyGemVersion(gemName)
+      fmt.Printf("%s: %s\n", gem.Name, gem.Version)
     } else if queryNodePkg {
       fmt.Println("node")
     } else {

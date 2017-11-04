e8ebf1041c7d88fbbdf427945b54c4f8ac8e45a3
Arunvel Sriram <arunvelsriram@gmail.com>
2017-11-04 20:41:45 +0530

    Use correct file name of goreleaser config

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-curl -sL https://git.io/goreleaser | bash -s -- --config release.yml --release-notes release-notes/$TRAVIS_TAG.md --debug
+curl -sL https://git.io/goreleaser | bash -s -- --config .release.yml --release-notes release-notes/$TRAVIS_TAG.md --debug

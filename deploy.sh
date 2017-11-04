cb3485d91cb8385b590e523ca0edc55231130da6
Arunvel Sriram <arunvelsriram@gmail.com>
2017-11-04 20:35:10 +0530

    Fixing build

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-curl -sL https://git.io/goreleaser | bash -s -- --config release.yml --release-notes release-notes/$TRAVIS_TAG.md
+curl -sL https://git.io/goreleaser | bash -s -- --config release.yml --release-notes release-notes/$TRAVIS_TAG.md --debug
